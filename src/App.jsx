f2b8605e4776ec2ba467f3176edef7df7d081f86
Aafrin777 <159065214+Aafrin777@users.noreply.github.com>
2025-05-15 18:45:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Use Routes instead of Switch
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="hero-section">
+        <p>Subscribe to </p>
+        <h1>Thapa Technical Home Page</h1>
+      </section>
+    </>
+  );
+};
+
+const About = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="hero-section">
+        <p>Welcome to </p>
+        <h1>Thapa Technical About Page</h1>
+      </section>
+    </>
+  );
+};
+
+const Service = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="hero-section">
+        <p>Welcome to </p>
+        <h1>Thapa Technical Service Page</h1>
+      </section>
+    </>
+  );
+};
+
+const Contact = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="hero-section">
+        <p>Welcome to </p>
+        <h1>Thapa Technical Contact Page</h1>
+      </section>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
